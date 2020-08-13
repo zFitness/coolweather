06714883d6f38a2ad7fea6ac8356d4864a9eb91a
zfitness <zhengmiaode@hotmail.com>
2020-08-14 07:29:32 +0800

    database

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,37 @@
+package com.coolweather.android.db;
+
+/**
+ * 省
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    /**
+     * 省的代号
+     */
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
